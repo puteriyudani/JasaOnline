65258e023543690148480803bb51fda63e4b8678
Puteri Yudani <puteriyudani@gmail.com>
2022-06-18 12:29:08 +0700

    Menampilkan data jasa user kedalam fragment jasa

--- app/src/main/java/com/puteriyudani/jasaonline/fragments/JasaFragment.kt
+++ app/src/main/java/com/puteriyudani/jasaonline/fragments/JasaFragment.kt
@@ -1,11 +1,24 @@
 package com.puteriyudani.jasaonline.fragments
 
+import android.app.ProgressDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.puteriyudani.jasaonline.R
+import com.puteriyudani.jasaonline.adapters.JasaAdapter
+import com.puteriyudani.jasaonline.helpers.SessionHandler
+import com.puteriyudani.jasaonline.models.Jasa
+import com.puteriyudani.jasaonline.models.JasaResponse
+import com.puteriyudani.jasaonline.services.JasaService
+import com.puteriyudani.jasaonline.services.ServiceBuilder
+import kotlinx.android.synthetic.main.fragment_jasa.view.*
+import retrofit2.Call
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +31,65 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class JasaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    lateinit var rvData: RecyclerView
+    lateinit var session : SessionHandler
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_jasa, container, false)
+        session = SessionHandler(requireContext())
+        val rootView = inflater.inflate(R.layout.fragment_jasa, container,
+            false)
+        rvData = rootView.findViewById(R.id.rvData)
+        return rootView
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment JasaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            JasaFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.rvData.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+    // override method dari onresume
+    override fun onResume() {
+        super.onResume()
+        loadService()
+    }
+    private fun loadService() {
+        val loading = ProgressDialog(context)
+        loading.setMessage("Meload Jasa Pengguna...")
+        loading.show()
+        val jasaService: JasaService =
+            ServiceBuilder.buildService(JasaService::class.java)
+        val requestCall: Call<JasaResponse> =
+            jasaService.getJasaUser(session.getUserId())
+        requestCall.enqueue(object : retrofit2.Callback<JasaResponse>{
+            override fun onFailure(call: Call<JasaResponse>, t: Throwable)
+            {
+                loading.dismiss()
+                Toast.makeText(context, "Error terjadi ketika sedang mengambil data jasa: " + t.toString(), Toast.LENGTH_LONG).show()
+            }
+            override fun onResponse(
+                call: Call<JasaResponse>,
+                response: Response<JasaResponse>
+            ) {
+                loading.dismiss()
+                if(!response.body()?.error!!) {
+                    val serveicesResponse: JasaResponse? = response.body()
+                    serveicesResponse?.let {
+                        val daftarJasa: List<Jasa> =
+                            serveicesResponse.data
+                        val jasaAdapter = JasaAdapter(daftarJasa) {
+                                service ->
+                            Toast.makeText(context, "service clicked ${service.namaJasa}", Toast.LENGTH_SHORT).show()
+                        }
+                        jasaAdapter.notifyDataSetChanged()
+                        rvData.adapter = jasaAdapter
+                    }
+                }else{
+                    Toast.makeText(context, "Gagal menampilkan data jasa: " + response.body()?.message, Toast.LENGTH_LONG).show()
                 }
             }
+        });
     }
 }
--- app/src/main/java/com/puteriyudani/jasaonline/services/JasaService.kt
+++ app/src/main/java/com/puteriyudani/jasaonline/services/JasaService.kt
@@ -3,8 +3,14 @@ package com.puteriyudani.jasaonline.services
 import com.puteriyudani.jasaonline.models.JasaResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface JasaService {
     @GET("services")
     fun getJasa() : Call<JasaResponse>
+
+    @GET("services/{id}")
+    fun getJasaUser(
+        @Path("id") id: Int
+    ) : Call<JasaResponse>
 }
